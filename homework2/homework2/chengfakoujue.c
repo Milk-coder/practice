5ac105bb58a8b87be0ed367a2273a28ec900b2cc
Milk-coder <mcp971017@163.com>
2019-09-19 15:41:15 +0800

    乘法口诀表

---
+++ homework2/homework2/chengfakoujue.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int i = 1;
+	while (i <= 9){
+		int j = 1;
+		while (j <= i){
+			printf("%d * %d = %d ", i, j, i * j);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	system("pause");
+	return 0;
+}
